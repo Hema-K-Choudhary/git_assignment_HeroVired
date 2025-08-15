98399d993f9d9bc09db50b7bb9b43379d6a7f5bc
Hema-K-Choudhary <tutee4ever@gmail.com>
2025-08-15 20:11:54 +0530

    Fix for div by zero added

--- CalculatorPlus/main.py
+++ CalculatorPlus/main.py
@@ -16,8 +16,10 @@ class Calculator:
 
    def divide(self, a, b):
 
-      return a / b
+      if b == 0:
+         raise ValueError("Cannot divide by zero.")
 
+      return a / b
 
    def square_root(self, x):
 
